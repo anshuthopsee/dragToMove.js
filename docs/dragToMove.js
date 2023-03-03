4dc40b55b443abca2704bd17ca2ee571dd483277
anshuthopsee <79682606+anshuthopsee@users.noreply.github.com>
2023-03-03 03:42:54 +0000

    Update dragToMove.js

--- docs/dragToMove.js
+++ docs/dragToMove.js
@@ -7,14 +7,14 @@ export function dragToMove(div, handle) {
     let startY;
 
     handle.addEventListener(isTouch ? "touchstart" : "mousedown", (e) => {
-        e.preventDefault();
+        e.stopPropogation();
         mousedown = true;
         offsetX = e.touches ? e.touches[0].clientX - e.target.getBoundingClientRect().left: e.offsetX
         offsetY = e.touches ? e.touches[0].clientY - e.target.getBoundingClientRect().top : e.offsetY
     });
 
     document.addEventListener(isTouch ? "touchmove" : "mousemove", (e) => {
-        e.preventDefault()
+        e.preventDefault();
         e = e.touches ? e.touches[0] : e;
         startX = e.clientX-offsetX;
         startY = e.clientY-offsetY;
